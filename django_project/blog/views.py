8890fb0cd1cf945de556129946c68f7d00fe14f6
abdullahi <jan.ab1517@gmail.com>
2023-09-18 08:37:38 +0100

    create demo date and accessed using context in views and by using for loop displayed all.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 
+
+# demo date
+
+posts = [
+    {
+        'author' : 'hamed',
+        'content' : 'first post 1',
+        'title' : 'post content first',
+        'date_posted' : 'August 27, 2018'
+    },
+    {
+        'author' : 'jan',
+        'content' : 'second post 2',
+        'title' : 'post content second',
+        'date_posted' : 'july 21, 2019'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
