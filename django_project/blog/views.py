7035c753c61a01366714bde9fffa91ae9d59ce17
abdullahi <jan.ab1517@gmail.com>
2023-09-24 13:49:14 +0100

    add user post update

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
@@ -20,6 +20,14 @@ class PostDetailView(DetailView):
     model = Post
 
 class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
     model = Post
     fields = ['title', 'content']
 
