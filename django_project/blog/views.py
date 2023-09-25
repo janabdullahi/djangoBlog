1f6bb0b838e42d4c9d941e6e393631049619ef92
abdullahi <jan.ab1517@gmail.com>
2023-09-25 07:32:33 +0100

    avoiding other users to update the others post by adding UserPAssesTestMixin

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 def home(request):
@@ -27,13 +27,20 @@ class PostCreateView(LoginRequiredMixin,CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object() # current post
+        if self.request.user == post.author: # if current user is post author
+            return True
+        return False
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
